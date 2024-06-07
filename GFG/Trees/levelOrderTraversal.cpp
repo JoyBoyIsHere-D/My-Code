eeebd71e15f3765d042aee953b054619fb12d9f1
JoyBoyIsHere-D <jayanshujubujena@gmail.com>
2024-06-07 20:16:00 +0530

    trees

--- GFG/Trees/levelOrderTraversal.cpp
+++ GFG/Trees/levelOrderTraversal.cpp
@@ -68,14 +68,14 @@ void printLevel3(Node *root){
     }
 }
 int main(){
-    Node *root=new Node(10);
-	root->left=new Node(20);
-	root->right=new Node(30);
-	root->left->left=new Node(40);
-	root->left->right=new Node(50);
-	root->right->left=new Node(60);
-	root->right->right=new Node(70);
+    Node *root=new Node(3);
+	root->left=new Node(9);
+	root->right=new Node(20);
+	// root->left->left=new Node(40);
+	// root->left->right=new Node(50);
+	root->right->left=new Node(15);
+	root->right->right=new Node(7);
 	
-	printLevel3(root);
+	printLevel2(root);
     return 0;
 }
