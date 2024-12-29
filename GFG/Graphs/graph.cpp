f2e612a91e3fcb106e1ff2891a24e5552f3db0e1
JoyBoyIsHere-D <jayanshujubujena@gmail.com>
2024-12-29 14:56:23 +0530

    Graph

---
+++ GFG/Graphs/graph.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define int long long
+#define endl '\n'
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+const int z=1e9+7;
+const int m1=1e7+10;
+const int m2=998244353;
+
+void solve(){
+    
+}
+
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    //freopen("a.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
+    int t=1;
+    cin>>t;
+    for(int i=1;i<=t;i++){
+        //cout<<"Case #"<<i<<": ";
+        solve();
+    }
+    return 0;
+}
